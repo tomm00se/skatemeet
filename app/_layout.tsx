bc000aaa7125af3466ee6140f8b8aec7b21f8102
Tommy Bowden <tom@moose17.dev>
2026-02-11 13:46:03 +0000

    wrapped app in auth provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,29 @@
+import AuthProvider from "@/providers/auth-provider";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: "index", headerShown: false }}
-      />
-      <Stack.Screen
-        name="LoginScreen"
-        options={{ title: "Login", headerShown: false }}
-      />
-      <Stack.Screen
-        name="SignUpScreen"
-        options={{ title: "Sign Up", headerShown: false }}
-      />
-    </Stack>
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ title: "index", headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          options={{ title: "Login", headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          options={{ title: "Sign Up", headerShown: false }}
+        />
+      </Stack>
+    </AuthProvider>
   );
 }
