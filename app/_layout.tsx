45bf42cc6bd69d1e7f8a26b79107e3613ac3d8ea
Tommy Bowden <tom@moose17.dev>
2026-02-11 19:35:01 +0000

    update layout to handle root navigation conditionally based on authcontext

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,48 @@
+import { useAuthContext } from "@/lib/context/use-auth-context";
 import AuthProvider from "@/providers/auth-provider";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
-SplashScreen.setOptions({
-  duration: 1000,
-  fade: true,
-});
+SplashScreen.preventAutoHideAsync();
+
+const RootNavigation = () => {
+  const { session, isLoadingAuthContext, profile } = useAuthContext();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoadingAuthContext) return;
+
+    const inAuthGroup = segments[0] === "(auth)";
+    const inOnboardingGroup = segments[0] === "(onboarding)";
+
+    if (!session) {
+      //If the user is not signed in, restrict them to login and sign up
+      router.replace("/Login");
+    } else if (profile && !profile.onboarding_completed) {
+      router.replace("/Onboarding");
+    } else if (session && (inAuthGroup || segments[0] === undefined)) {
+      router.replace("/(tabs)");
+    }
+
+    SplashScreen.hideAsync();
+  }, [session, isLoadingAuthContext, profile, segments, router]);
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="Login" options={{ headerShown: false }} />
+      <Stack.Screen name="SignUp" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
 
 export default function Layout() {
   return (
     <AuthProvider>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ title: "index", headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{ title: "Login", headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          options={{ title: "Sign Up", headerShown: false }}
-        />
-      </Stack>
+      <RootNavigation />
     </AuthProvider>
   );
 }
